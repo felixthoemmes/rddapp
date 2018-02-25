c62587641c60923c3c589f4375a532ba7d14b437
Irena Papst <ip98@cornell.edu>
2018-02-24 20:15:39 -0500

    first pass at including help info
    * just used includeMarkdown() on full help page doc
    * no way to add table of contents, so need to explore other ways of creating this page

--- inst/shinyrdd/ui_elements/page_help.R
+++ inst/shinyrdd/ui_elements/page_help.R
@@ -1,10 +1,1 @@
-navlistPanel(widths = c(3,9),
-  "Section A",
-  tabPanel("Topic 1", 
-    helpText("I help you on Topic 1.")),
-  tabPanel("Topic 2",
-    helpText("I help you on Topic 2.")),
-  "Section B",
-  tabPanel("Topic 3",
-    helpText("I help you on Topic 3."))
-)
+includeMarkdown("help_page_content/help_page_full.Rmd")
