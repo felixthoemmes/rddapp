5381db464fcc3ebeed86ba4dbce3caa9bc866f8e
Irena Papst <ip98@cornell.edu>
2018-02-24 20:55:38 -0500

    attempt 2 at creating help page
    * generate html using rmarkdown package
    * need to strip everything generated outside <body> </body> tags to get the html to display in shiny (otherwise help page doesn't even show up in the menu)

--- inst/shinyrdd/ui_elements/page_help.R
+++ inst/shinyrdd/ui_elements/page_help.R
@@ -1,1 +1,12 @@
-includeMarkdown("help_page_content/help_page_full.Rmd")
+## Attempt 1: no TOC
+##includeMarkdown("help_page_content/help_page_full.Rmd")
+
+## Attempt 2: works if we only include what's between the
+## <body> </body> tags
+# library("rmarkdown")
+filename <- "help_page_content/help_page_full"
+# render(paste0(filename,".Rmd"), html_document(toc = TRUE, toc_depth = 3))
+includeHTML(paste0(filename,".html"))
+## Try to strip all but what's in html body 
+## some characters in the regex need to be stripped
+# system("sed 's/^.*\(<body>.*</body>\).*$/\1/' help_page_full.html > test.txt")
