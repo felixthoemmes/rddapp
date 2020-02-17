0110548af680675aa24fee2d47d18d1d519b2789
Felix Thoemmes <felix.thoemmes@gmail.com>
2020-02-16 20:15:46 -0500

    Update mrd_impute.R
    Smaller sample sizes so that hopefully example will run in less than 10 seconds to fulfill CRAN rules

--- R/mrd_impute.R
+++ R/mrd_impute.R
@@ -78,11 +78,11 @@
 #' @export
 #'
 #' @examples
-#' x1 <- runif(1000, -1, 1)
-#' x2 <- runif(1000, -1, 1)
-#' cov <- rnorm(1000)
-#' y <- 3 + 2 * (x1 >= 0) + 3 * cov + 10 * (x2 >= 0) + rnorm(1000)
-#' group <- rep(1:10, each = 100)
+#' x1 <- runif(500, -1, 1)
+#' x2 <- runif(500, -1, 1)
+#' cov <- rnorm(500)
+#' y <- 3 + 2 * (x1 >= 0) + 3 * cov + 10 * (x2 >= 0) + rnorm(500)
+#' group <- rep(1:10, each = 50)
 #' # centering
 #' mrd_impute(y ~ x1 + x2 | cov, impute = group, method = "center", t.design = c("geq", "geq"))
 #' # univariate
