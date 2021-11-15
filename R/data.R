4fc7b54df50552b814d52806e99d4a40c75064c8
Kim Hochstedler <kimhochstedler@gmail.com>
2021-11-15 10:51:45 -0500

    update CARE documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Carolina Abecedarian Project and the Carolina Approach to Responsive Education (CARE), 1972-1992
 #'
-#' A subset of children of the randomized controlled CARE trial on early childhood intervention. 
+#' A dataset containing a subset of children from the CARE trial on early childhood intervention. 
 #' The randomized controlled trial was subsetted to mimic a regression-discontinuity design in which 
 #' treatment was assigned only to mothers whose IQ was smaller than 85.
 #' 
@@ -9,7 +9,7 @@
 #' \describe{
 #'   \item{SUBJECT}{Unique ID variable}
 #'   \item{DC_TRT}{Day Care (Preschool) Treatment Group, 1 = Treatment, 0 = Control}
-#'   \item{APGAR5}{APGAR score at 5 minutes after birth}
+#'   \item{APGAR5}{APGAR ("Appearance, Pulse, Grimace, Activity, and Respiration.") score at 5 minutes after birth}
 #'   \item{MOMWAIS0}{Biological mother's WAIS (Wechsler Adult Intelligence Scale) 
 #'     full-scale score at subject's birth}
 #'   \item{SBIQ48}{Subject's Stanford Binet IQ score at 48 months}
@@ -17,5 +17,10 @@
 #'
 #' @source \url{http://www.icpsr.umich.edu/icpsrweb/ICPSR/studies/4091}
 #' @source \url{http://www.researchconnections.org/childcare/studies/4091/version/1}
+#' 
+#' @examples 
+#' data("CARE")
+#' head(CARE)
+#' 
 
 "CARE"
