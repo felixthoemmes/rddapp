1ecaee05b98f45eaccc44e69d93f03b3cc6f31d0
Felix J. Thoemmes <fjt36@cornell.edu>
2025-07-23 10:30:33 -0400

    Clarify vcovHC reference in documentatin
    Updated Roxygen documentation in mrd_est.R, mrd_impute.R, rd_est.R, and rd_impute.R to specify vcovHC as from the sandwich package using \link[sandwich]{vcovHC} for improved clarity.

--- R/mrd_impute.R
+++ R/mrd_impute.R
@@ -35,7 +35,7 @@
 #'   \code{"triweight"}, \code{"tricube"}, and \code{"cosine"}.
 #' @param se.type This specifies the robust standard error calculation method to use,
 #'   from the "sandwich" package. Options are,
-#'   as in \code{\link{vcovHC}}, \code{"HC3"}, \code{"const"}, \code{"HC"}, \code{"HC0"}, 
+#'   as in \code{\link[sandwich]{vcovHC}}, \code{"HC3"}, \code{"const"}, \code{"HC"}, \code{"HC0"}, 
 #'   \code{"HC1"}, \code{"HC2"}, \code{"HC4"}, \code{"HC4m"}, \code{"HC5"}.
 #'   The default is \code{"HC1"}. This option is overridden by \code{cluster}.
 #' @param cluster An optional vector of length n specifying clusters within which the errors are assumed
